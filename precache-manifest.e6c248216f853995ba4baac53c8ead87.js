73654308e95812d43653a04bbe32827a80129ee4
Maksym Tarasenko <max.tarasenko.slip@gmail.com>
2020-07-17 12:58:02 +0300

    Updates

---
+++ precache-manifest.e6c248216f853995ba4baac53c8ead87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b3fb23c290c9e90961439a32fd7997",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "6b7a8d108df32594e03e",
+    "url": "/goit-react-hw-03-phonebook/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "29fc8f95e9159c4dc1ae",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.f23f439b.chunk.css"
+  },
+  {
+    "revision": "6b7a8d108df32594e03e",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.04e24cfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.04e24cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29fc8f95e9159c4dc1ae",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.8be80ef3.chunk.js"
+  },
+  {
+    "revision": "a8186ef5d4c20ea0b070",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.76aa6eee.js"
+  }
+]);
